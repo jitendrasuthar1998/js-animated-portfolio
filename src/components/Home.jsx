ac6890f94b27787438bab16ed37428b6d9ef0a2f
jitendrasuthar1998 <jitendrasuthar995@gmail.com>
2024-08-05 01:29:19 +0530

    wip on home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import resume from "../pdf/Jitendra Suthar Resume.pdf"
+import hero from "../images/hero/hero.avif"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="container home">
+      <div className="left">
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, commodi!</h1>
+        <a className="btn btn-outline-warning" href={resume} download="jitendra-suthar-resume.pdf">Download Resume</a>
+      </div>
+      <div className="right">
+    <div className="img">
+    <img src={hero} alt="hero"/>
+    </div>
+      </div>
+    </div>
   )
 }
 
